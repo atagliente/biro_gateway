24a42e032151a8237acd9a090d3d2ee65114b7f0
atagliente <antoniotagliente@aol.com>
2022-11-05 20:18:09 +0100

    add RBAC with keycloak and SecurityConfig | Worked!

--- src/main/java/it/biro/biro_gateway/SecurityConfig.java
+++ src/main/java/it/biro/biro_gateway/SecurityConfig.java
@@ -1,50 +1,59 @@
 package it.biro.biro_gateway;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import java.util.*;
 
-@Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        SecurityWebFilterChain swfc = null;
         http
                 .csrf()
                 .disable()
                 .authorizeExchange()
-                .pathMatchers("/actuator/**", "/","/logout.html")
-                .permitAll()
-                .and()
-                .authorizeExchange()
-                .anyExchange()
-                .authenticated()
-                .and()
-//                .oauth2Login() // to redirect to oauth2 login page.
-//                .and()
-//                .logout()
-//                .logoutSuccessHandler(handler)
-//                .and()
+                .pathMatchers("/user").hasAuthority("user_read")
+                .pathMatchers("/admin").hasAuthority("admin_read")
+                .anyExchange().permitAll().and()
                 .oauth2ResourceServer()
-                .jwt();
-
-        return http.build();
+                .jwt(jwt -> {
+                    System.out.println(jwt);
+                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter());
+                });
+        swfc = http.build();
+        return swfc;
     }
 
-//    @Bean
-//    public ServerLogoutSuccessHandler keycloakLogoutSuccessHandler(ReactiveClientRegistrationRepository repository) {
-//
-//        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
-//                new OidcClientInitiatedServerLogoutSuccessHandler(repository);
-//        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/logout.html");
-//
-//        return oidcLogoutSuccessHandler;
-//    }
+    private Converter<Jwt, ? extends Mono<? extends AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        ReactiveJwtAuthenticationConverter jwtConverter = new ReactiveJwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new RealmRoleConverter());
+        return jwtConverter;
+    }
 
+    public class RealmRoleConverter implements Converter<Jwt, Flux<GrantedAuthority>> {
+        @Override
+        public Flux<GrantedAuthority> convert(Jwt jwt) {
+            final Map<String, List<String>> realmAccess = (Map<String, List<String>>) jwt.getClaims().get("realm_access");
+            System.out.println("\n\n\n" + realmAccess.toString() + "\n\n\n");
+            return Flux.fromStream(realmAccess.get("roles").stream()
+                    .map(SimpleGrantedAuthority::new));
+        }
+    }
 }
--- src/main/java/it/biro/biro_gateway/controllers/Controller.java
+++ src/main/java/it/biro/biro_gateway/controllers/Controller.java
@@ -1,18 +1,30 @@
 package it.biro.biro_gateway.controllers;
 
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.security.Principal;
 
 @RestController
 public class Controller extends org.springframework.web.cors.CorsConfiguration {
-
     @CrossOrigin
     @GetMapping("/user")
-    public String index(Principal principal) {
+    public String u_index(Principal principal) {
         return principal.getName();
+    }
+
+    @CrossOrigin
+    @GetMapping("/admin")
+    public String a_index(Principal principal) {
+        return principal.getName();
+    }
+
+    @CrossOrigin
+    @GetMapping("/test")
+    public String s_index(Principal principal) {
+        JwtAuthenticationToken jwtAT = (JwtAuthenticationToken) principal;
+        return jwtAT.getTokenAttributes().toString();
     }
 
 }
